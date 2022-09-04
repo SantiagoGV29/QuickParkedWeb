93a01b20aed881bcc8bfa85a5bd7859642e556f5
txusdavid27 <jetraslavina@javeriana.edu.co>
2022-09-04 14:30:45 -0500

    add bootstrap
    See manual installation https://ng-bootstrap.github.io/#/getting-started

--- QuickParked-Font/src/app/app.module.ts
+++ QuickParked-Font/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbPaginationModule,
+    NgbAlertModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
