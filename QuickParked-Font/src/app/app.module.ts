8ae9c73d0207e2daab36457c9399433f6d3dbb82
txusdavid27 <jetraslavina@javeriana.edu.co>
2022-09-04 16:38:03 -0500

    Primer Uso de Estilos
    Agregada la principal editable, listo para componentes modelos y servicios

--- QuickParked-Font/src/app/app.module.ts
+++ QuickParked-Font/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import{FormsModule}from '@angular/forms';
+
+import{HttpClientModule}from '@angular/common/http'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule,
-    NgbPaginationModule,
-    NgbAlertModule,
-    NgbModule
+    HttpClientModule,
+    NgbPaginationModule
   ],
-  providers: [],
+  providers: [],//Luego se coloca el service
   bootstrap: [AppComponent]
 })
 export class AppModule { }
