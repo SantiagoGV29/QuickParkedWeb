dce4d7b98298358b0adc6af170d24607aa857b0f
dani09barreto <danielf.barreto@javeriana.edu.co>
2022-09-06 20:40:10 -0500

    se añaden anotaciones service en el back

--- QuickParked-Back/src/main/java/com/example/quickparkedback/Service/ISlotParkingService.java
+++ QuickParked-Back/src/main/java/com/example/quickparkedback/Service/ISlotParkingService.java
@@ -1,7 +1,9 @@
 package com.example.quickparkedback.Service;
 
 import com.example.quickparkedback.Model.Slotparking;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ISlotParkingService {
     Slotparking insertSlot(Slotparking slot);
     Boolean DeleteSlot(Integer id);
--- QuickParked-Back/src/main/java/com/example/quickparkedback/Service/ITypeVehicleService.java
+++ QuickParked-Back/src/main/java/com/example/quickparkedback/Service/ITypeVehicleService.java
@@ -1,7 +1,9 @@
 package com.example.quickparkedback.Service;
 
 import com.example.quickparkedback.Model.Typevehicle;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ITypeVehicleService {
     Typevehicle insertType(Typevehicle type);
     Boolean deleteType(Integer id);
